9d0d49b23c11e4252cb4ac1802c02f724d538628
maytag34 <connor_mlb34@yahoo.com>
2019-07-14 18:35:51 -0400

    Added hand functionality

--- blackjack.py
+++ blackjack.py
@@ -2,12 +2,24 @@
 This is a program that imitates a simplified casino blackjack game
 """
 import deck
+import hand
 
 # A variable to determine if the game is being played
 playing = True
 
 test_deck = deck.Deck()
-print(test_deck)
 test_deck.shuffle()
-print(test_deck)
+
+test_player = hand.Hand()
+
+pulled_card = test_deck.deal()
+print(pulled_card)
+test_player.add_card(pulled_card)
+print(test_player.hand_value)
+
+pulled_card2 = test_deck.deal()
+print(pulled_card2)
+test_player.add_card(pulled_card2)
+print(test_player.hand_value)
+
 
--- hand.py
+++ hand.py
@@ -1,5 +1,26 @@
 """This script holds the definition of the Hand class"""
+import card
+import deck
 
 
 class Hand:
-    pass
+    def __init__(self):
+        self.cards = []  # A list of the current cards in the user's hand
+        self.hand_value = 0  # The actual value of the user's hand
+        self.num_aces = 0  # Keeps track of the number of aces that the user has
+
+    def __len__(self):
+        return len(self.cards)
+
+    def add_card(self, new_card):
+        self.cards.append(new_card)
+        self.hand_value += deck.values[new_card.rank]
+
+        if new_card.rank == 'Ace':
+            self.num_aces += 1
+
+    def adjust_for_ace(self):
+        # If the users hand value is greater than 21 but the user still has an ace, you need to re-adjust the hand value
+        while self.hand_value > 21 and self.num_aces:
+            self.hand_value -= 10
+            self.num_aces -= 1
